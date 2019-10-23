f19f7372780e1573fcab475fa6f73c554795d098
csaflip <colin.adams@ufl.edu>
2019-10-22 22:48:54 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Guitar-Learning-Companion',
+    version='1.0',
+    packages=['PySimpleGUI'],
+    url='',
+    license='',
+    author='csaflip',
+    author_email='',
+    description=''
+)
